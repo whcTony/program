a3b9476eabcded2c9a832093bad499f06e720e61
tony <wanghaocbjx@sina.com>
2017-09-02 10:56:17 +0800

    完成启用和注销教师功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -9,10 +9,7 @@ define(['jquery','template','bootstrap'],function($,template){
              var html = template('teacherTpl',{list:data.result});
              $('#teacherInfo').html(html);
              //绑定预览单机事件
-             $('.preview').click(function(){
-             	//获取当前记录ID
-             	var td = $(this).closest('td');
-             	var tcId = td.attr('data-tcId');
+             $('.preview').click(function(){	
              	//根据ID查询数据
              	$.ajax({
              		type:'get',
@@ -26,11 +23,54 @@ define(['jquery','template','bootstrap'],function($,template){
              			//显示弹窗
              			$('#teacherModal').modal();
              		}
-             	})
-             	
-             })
-		}
-	});
+             	}); });
+               //控制启用和注销
+                $('.eod').click(function(){
+             		             //获取当前记录ID
+                        	var td = $(this).closest('td');
+             	            var tcId = td.attr('data-tcId');
+             	            var tcStatus = td.attr('data-status');
+             	            //缓存this
+             	            var that = this;
+             	            //调用接口
+             	            $.ajax({
+             	            	type:'post',
+             	            	url:'/api/teacher/handle',
+             	            	data:{tc_id : tcId,tc_status:tcStatus},
+                                dataType:'json',
+                                success:function(data){
+                                     if(data.code == 200){
+                                     	//修改当前状态
+                                       td.attr('data-status',data.result.tc_status);
+                                     //修改文字信息
+                                     if(data.result.tc_status ==0){
+                                     	$(that).html('注 销')
+                                     }else{
+                                     	$(that).html('启 用')
+                                     }
+                                   }
+                                }
+             	            });
+             	        });
+             
+                
+		                 }
+	                   
 
 
-});
+                  });
+	 });
+
+
+
+
+
+
+
+
+
+
+               
+
+
+ 
